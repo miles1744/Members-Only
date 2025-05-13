f900f2199922d8e3320da564ef9e8fbe390d4a3e
Miles1744 <mithomtab@gmail.com>
2025-05-13 09:56:52 -0400

    New

---
+++ client/src/Login.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import './App.css'
+
+function Login() {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:3000/login", formData);
+      console.log("Logged in:", res.data);
+    } catch (err) {
+      console.error("Login failed:", err.response?.data || err.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return ( 
+    <>
+      {locals.user ? (
+        <>
+          <h1>Welcome Back {user.username}</h1>
+          <a href="/log-out"><button>Log Out</button></a> 
+        </>
+      ):(
+        <>
+          <h1>Please Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <label>Username</label>
+            <input type="text" name="username" value={formData.username} onChange={handleChange} placeholder="Username" />
+            <label>Password</label>
+            <input type="password" name="password" value={formData.password} onChange={handleChange} placeholder="Password" />
+            <button type="submit">Login</button>
+          </form>
+        </>
+      )}
+    </>
+  )
+}
+
+export default Login;
