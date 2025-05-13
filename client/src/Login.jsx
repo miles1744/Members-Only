74371483e3dccbe7c25c57e843339eb97aed6a87
Miles1744 <mithomtab@gmail.com>
2025-05-13 09:11:35 -0400

    New

---
+++ client/src/Login.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function Login() {
+
+  return ( 
+    <>
+      <form>
+        <input type="text" name="username" placeholder="Username" required />
+        <input type="password" name="password" placeholder='"passw'
+      </form>
+    </>
+  )
+}
+
+export default Login;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Login from './Login.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
