a96979cb03b7eec10772eb82619918785ae086e4
Miles1744 <mithomtab@gmail.com>
2025-05-14 14:50:46 -0400

    New

--- server/db/init_db.js
+++ server/db/init_db.js
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS messages (
     title VARCHAR(255) NOT NULL,
     body TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 `;
 
 async function main() {
